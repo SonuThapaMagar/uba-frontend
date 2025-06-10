4745f544522dca4ab90c85796e54016588fdbbc7
sonu <sonumagar787@gmail.com>
2025-06-10 14:44:36 +0545

    gql api integrated userlist

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,49 +1,47 @@
-import { Resolver, Query, Arg, Mutation } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
 import { User } from '../entities/User';
 import { SignupInput } from '../entities/AuthInput';
 import { AppDataSource } from '../config/database';
 
 @Resolver()
 export class UserResolver {
-    @Query(() => User, { nullable: true })
-    async user(@Arg('id') id: string): Promise<User | null> {
-        return AppDataSource.getRepository(User).findOneBy({ id });
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    return AppDataSource.getRepository(User).find();
+  }
+
+  @Query(() => User, { nullable: true })
+  async user(@Arg('id') id: string): Promise<User | null> {
+    return AppDataSource.getRepository(User).findOneBy({ id });
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Arg('id') id: string,
+    @Arg('input') input: SignupInput
+  ): Promise<User> {
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneBy({ id });
+    if (!user) {
+      throw new Error('User not found');
     }
-
-    @Query(() => [User])
-    async users(): Promise<User[]> {
-        return AppDataSource.getRepository(User).find();
+    user.email = input.email;
+    user.fname = input.fname;
+    user.lname = input.lname;
+    if (input.password) {
+      user.password = await import('bcryptjs').then(bcrypt => bcrypt.hash(input.password, 10));
     }
+    return userRepository.save(user);
+  }
 
-    @Mutation(() => User)
-    async updateUser(
-        @Arg('id') id: string,
-        @Arg('input') input: SignupInput
-    ): Promise<User> {
-        const userRepository = AppDataSource.getRepository(User);
-        const user = await userRepository.findOneBy({ id });
-        
-        if (!user) {
-            throw new Error('User not found');
-        }
-
-        user.email = input.email;
-        user.fname = input.fname;
-        user.lname = input.lname;
-
-        return userRepository.save(user);
+  @Mutation(() => String)
+  async deleteUser(@Arg('id') id: string): Promise<string> {
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneBy({ id });
+    if (!user) {
+      throw new Error('User not found');
     }
-
-    @Mutation(() => String)
-    async deleteUser(@Arg('id') id: string): Promise<string> {
-        const userRepository = AppDataSource.getRepository(User);
-        const user = await userRepository.findOneBy({ id });
-        
-        if (!user) {
-            throw new Error('User not found');
-        }
-
-        await userRepository.remove(user);
-        return id;
-    }
+    await userRepository.remove(user);
+    return id;
+  }
 }
