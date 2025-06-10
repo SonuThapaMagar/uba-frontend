da4c464f02b8d544f26f768200782582ed48e897
sonu <sonumagar787@gmail.com>
2025-06-10 13:09:31 +0545

    delete functionality

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -52,11 +52,6 @@ export const GET_USERS = gql`
 
 export const DELETE_USER = gql`
   mutation DeleteUser($id: String!) {
-    deleteUser(id: $id) {
-      id
-      email
-      fname
-      lname
-    }
+    deleteUser(id: $id)
   }
 `;
